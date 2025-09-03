8e1f231e80e816801c7dd5a85c5b1b7c1fcb66ec
gauravchauhan200 <gauravchauhan200@gmail.com>
2025-09-04 03:50:19 +0530

    UpdateDay10

--- Day11/AdvancedArrayMethods.ts
+++ Day11/AdvancedArrayMethods.ts
@@ -37,11 +37,94 @@
     // });
     // console.log(squaredNumbers);
 
-   //  Ex2: Double each number [1,2,3,4,5]--->[2,4,8,10]
+//  Ex2: Double each number [1,2,3,4,5]--->[2,4,8,10]
     
-   let numbers:number[]=[1,2,3,4,5];
+    //    let numbers:number[]=[1,2,3,4,5];
+        
+    //    let doubledNumber=numbers.map((element)=>{
+    //         return(element*2);
+    //     });
+    //     console.log(doubledNumber);
+
+    //if you have single return statement then currly bracket and return is optional and its valid only for arraw function
+
+
+    //3. fillter()
+
+    //Ex1.  Get the only even numbers from array
+
+        // let numbers:number[]=[1,2,3,4,5,6,7,8,9,10];
+
+        // let evenNumbers=numbers.filter((num)=>num%2==0);
+        // console.log(evenNumbers);
+ 
+      // Ex2 
+        //     let numbers:number[]=[1,2,3,4,5,6,7,8,9,10];
     
-   let doubledNumber=numbers.map((element)=>{
-        return(element*2);
+        //     let greaterNumbers=numbers.filter((num)=> num>3);
+        //     console.log(greaterNumbers);
+
+
+
+//Ex 4. reduce()
+    //syntax: array.reduce(function(accumulator,currentvalue,index,array){})
+    //Ex1 Get sum of all the elements in an array
+
+            // let total = 0;
+            // let numbers:number[]=[1,2,3,4,5];
+            // for(let i=0;i<numbers.length;i++)
+            // {
+            //     total=total+numbers[i];
+            // }
+            // console.log(total);
+
+    //using reduce method
+
+            // let numarray:number[]=[1,2,3,4,5];
+            
+            // let accumulatedvalue= numarray.reduce((total,element)=>
+            // {
+            //     return(total+element);
+            // });
+            // console.log(accumulatedvalue);
+
+
+
+//5. some() checks if any element satisfies a condition
+    //syntax: array.some(function(currentvalue,index,array){})
+
+    //Ex1 checks array contains negative values
+
+        // let numarray:number[]=[1,2,3,4,-1];
+
+        // let isPositive=numarray.some((element)=>{
+        //     return (element<0);
+        // });
+        // console.log("Does array contains any Negative values", isPositive);
+
+    //Ex 2: Check if any number is even
+
+        //     let numarray:number[]=[1,3,5,7,2];
+
+        //     let result=numarray.some(function(element)
+        // {
+        //     return(element%2==0);
+        // });
+        // console.log("Is any number even in the array :  ",result);
+
+//6. every()- checks if all elements satisfy the condition
+
+    //Ex1; check if all numbers are greater than one
+
+    let arraynum:number[]=[2,3,4,5,5,9];
+
+    let allGreater=arraynum.every(function(element){
+        return element>1;
     });
-    console.log(doubledNumber);
+    console.log(allGreater);
+
+
+
+
+
+
