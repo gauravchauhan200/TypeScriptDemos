f4d17c2236e27d5942a84c04b66f4a5a294044a5
gauravchauhan200 <gauravchauhan200@gmail.com>
2025-09-07 00:24:24 +0530

    Added13 and 14

--- Day13/Objects.ts
+++ Day13/Objects.ts
@@ -1,2 +1,34 @@
 
+//Object- object contains properties and behaviour.
+//object contains variables and methods
+//object is collection of key and value pairs.
 
+//Ex:
+
+//employee --name, desig, sal, dep
+//              bonus(),getemdetails(),setdetails()
+
+//student- name,sid,grade
+        //getdetails(),setdetails()
+
+       
+       
+ // Differents ways to create an object inJS/TS //
+ let employee ={
+
+    name:"John",
+    age:30,
+    salary:50000,
+    job:"Engineer",
+    getDetails:function():string{
+      //  console.log(this.name, this.age, this.salary,this.job);
+        return `${this.name} is a ${this.job} earning ${this.salary}` ;
+    }
+ }
+ console.log(typeof employee);
+
+ //accesing object= approach 1(using . notation)
+ console.log(employee.name,employee.salary,employee.job);
+ console.log(employee.getDetails());
+
+    
---
+++ Day7/Test.ts
@@ -0,0 +1,24 @@
+
+function add(a:number,b:number):number
+{
+    return a+b;
+}
+console.log(add(5,10));
+
+//--------------------------------------------------
+
+let multiply=function(c:number,d:number):number
+{
+    return c*d;
+}
+console.log(multiply(4,5));
+
+//----------------------------------------------------
+
+let square=(e:number):number=>
+{
+    return e*e;
+}
+console.log(square(4));
+
+//-------------------------------------------------
